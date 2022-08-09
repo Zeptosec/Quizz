94622edc103e1a66d27a971df7f994b31aed59d1
Zeptosec <armandasmatkevicius@gmail.com>
2022-08-09 19:31:40 +0300

    Task scheme

---
+++ backend/models/taskModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: [{
+        type: String,
+        required: true
+    }],
+    points: {
+        type: Number,
+        required: true
+    },
+    diffulty: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('task', taskSchema);
