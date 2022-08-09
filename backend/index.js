79440811be9aeb1209a0837312d49a12e2bb40e7
Zeptosec <armandasmatkevicius@gmail.com>
2022-08-09 18:55:44 +0300

    Starting from backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // for parsing json
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "We're good to go!" });
+});
+
+mongoose.connect(process.env.MONG_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'quiz',
+}).then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`running on port ${process.env.PORT || 4000}!`);
+    });
+}).catch(err => {
+    console.log(err);
+});
