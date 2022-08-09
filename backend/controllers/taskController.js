2c9bc7a22e0a16ead9913e268619bfc7f4b8c18d
Arnas <sniokaitisarnas@gmail.com>
2022-08-09 20:44:04 +0300

    Throws error

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,30 +1,30 @@
-const Task = require('../models/taskModel');
-const mongoose = require('mongoose');
+const Task = require("../models/taskModel");
+const mongoose = require("mongoose");
 
 // Create new tasks in database
 const createTask = async (req, res) => {
-    const { question, answer, points, difficulty } = req.body;
+  const { question, answer, points, difficulty } = req.body;
 
-    try {
-        const task = await Task.create({question, answer, points, difficulty});
-        res.status(200).json(task);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
+  try {
+    if (answer.length == 0) {
+      throw Error("Ad an answer");
+    } else {
+      const task = await Task.create({ question, answer, points, difficulty });
+      res.status(200).json(task);
     }
-}
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 // Get all tasks from database
-const getTasks = async (req, res) => {
-
-}
+const getTasks = async (req, res) => {};
 
 // Get a single task from database
-const getTask = async (req, res) => {
-
-}
+const getTask = async (req, res) => {};
 
 module.exports = {
-    createTask,
-    getTasks,
-    getTask
-}
+  createTask,
+  getTasks,
+  getTask,
+};
