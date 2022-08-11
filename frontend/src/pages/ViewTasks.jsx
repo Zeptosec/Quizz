70f694c2c0c07fb7072a6edbd2091c7393327fe8
Arnas <sniokaitisarnas@gmail.com>
2022-08-11 19:47:25 +0300

    idk what i did

--- frontend/src/pages/ViewTasks.jsx
+++ frontend/src/pages/ViewTasks.jsx
@@ -1,12 +1,23 @@
-
-
+import { useEffect } from "react";
+import { useState } from "react";
 const ViewTasks = () => {
-    
-    return (
-        <div className="viewTasks">
-            tasks
-        </div>
-    )
-}
+  const [tasks, setTasks] = useState([]);
+  const getTasks = async () => {
+    const res = await fetch("http://localhost:4000/api/tasks");
+    const json = await res.json();
+    setTasks(json);
+  };
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  return (
+    <div className="viewTasks">
+      {tasks.map((x) => (
+        <div>x.question</div>
+      ))}
+    </div>
+  );
+};
 
 export default ViewTasks;
